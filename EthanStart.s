4342c17f72d4a95eb768eacbcb6d290f05f4b2e8
BlackDog8815 <jilote@uwplatt.edu>
2026-05-07 12:39:50 -0500

    Create EthanStart.s
    Starting instructions for movement of the character in our game

---
+++ EthanStart.s
@@ -0,0 +1,37 @@
+.data
+	format: .asciz "%d"
+	input: .space 4
+_input_handler:
+	push {lr}
+
+	ldr r0, =format
+	ldr r1, =input
+	bl scanf	
+
+	cmp r1, #"w" @movement up
+	beq _up
+
+	cmp r1, #"a" @movement left
+	beq _left
+
+	cmp r1, #"s" @movement down
+	beq _down
+
+	cmp r1, #"d" @movement right
+	beq _right
+
+_up:
+	add r0, r0, #1
+	pop {lr}
+
+_left:
+	sub r1, r1, #1
+	pop {lr}
+
+_down:
+	sub r0, r0, #1
+	pop {lr}
+
+_right:
+	add r1, r1, #1
+	pop {lr}
